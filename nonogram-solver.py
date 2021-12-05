51c83e46905151ebc9c83ec180010858096e6fda
freesummerwind <liz-e-tta@yandex.ru>
2021-12-06 01:34:31 +0300

    add file reader function

---
+++ nonogram-solver.py
@@ -0,0 +1,43 @@
+
+
+def line_transformer(line, colors):
+    words_in_line = line.split()
+    elements = []
+    if len(words_in_line) > 0:
+        for word in words_in_line:
+            index = 0
+            for letter in word:
+                if letter.isdigit():
+                    index += 1
+                else:
+                    break
+            if index >= len(word) or word[index:] not in colors:
+                raise Exception('Incorrect input: no such color \'{0}\''.format(word[index:]))
+            elements.append((word[index:], int(word[:index])))
+    return elements
+
+
+def file_reader(path_to_file):
+    colors = []
+    lines = []
+    columns = []
+    colors_filled = False
+    lines_filled = False
+    with open(path_to_file) as file:
+        for line in file:
+            line = line.strip(whitespace)
+            if not colors_filled:
+                if len(line) < 9 or line[:8] != 'colors: ':
+                    raise Exception('Incorrect input: no colors')
+                colors = line[8:].split()
+                colors_filled = True
+            elif not lines_filled:
+                if line == 'lines:':
+                    continue
+                elif line == 'columns:':
+                    lines_filled = True
+                else:
+                    lines.append(line_transformer(line, colors))
+            else:
+                columns.append(line_transformer(line, colors))
+    return Nonogram(colors, lines, columns)
